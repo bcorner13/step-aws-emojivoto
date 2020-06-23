97ed0fb96929083b1416ee04a79e4da38db38098
Corner, Bradley <bcorner@kemper.com>
2020-06-23 19:58:28 -0400

    escape backslash

--- emojivoto.tf
+++ emojivoto.tf
@@ -17,7 +17,7 @@ provider "aws" {
 # Create an SSH key pair to connect to our instances
 resource "aws_key_pair" "terraform" {
   key_name   = "terraform-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCneX75UCGIDI2jibfaRAJGTEkT6K8DkbM1Z1n7GDOu0xexcUN8HHmyns90pEbNmR7PDjxyxfCHm7PCRRSDTJLuNcALqwN9sWiwqOoua/bKuvwMGMv+0hIJbSC9VhlZgRP6vQehhEGK+wgoouDwXiXizfvVPzKyrgbNm799Z9UoZEPMQOxOrxQp5tTtlhlUjlHsRbVlQaM025HvifxdZIEj/CtJ6dslS8Go2Joma3GIJZskCX/3K0vomOmWTq4n6MSqvGeL+rn7XgcKvs78AZGEtHhEU+3yIIz94e0mK0jC2ADyOlsVotN56RrXWf/OL5cyLvnxJZAOfvPuXLBCTudB unitrininc\usgbxc@KPIJAX81787"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCneX75UCGIDI2jibfaRAJGTEkT6K8DkbM1Z1n7GDOu0xexcUN8HHmyns90pEbNmR7PDjxyxfCHm7PCRRSDTJLuNcALqwN9sWiwqOoua/bKuvwMGMv+0hIJbSC9VhlZgRP6vQehhEGK+wgoouDwXiXizfvVPzKyrgbNm799Z9UoZEPMQOxOrxQp5tTtlhlUjlHsRbVlQaM025HvifxdZIEj/CtJ6dslS8Go2Joma3GIJZskCX/3K0vomOmWTq4n6MSqvGeL+rn7XgcKvs78AZGEtHhEU+3yIIz94e0mK0jC2ADyOlsVotN56RrXWf/OL5cyLvnxJZAOfvPuXLBCTudB unitrininc\\usgbxc@KPIJAX81787"
 }
 
 variable "ami" {
